569be52ac9690db18ba9a7ad9855d9ba8963c87f
Alex Emrick <akemrick@wpi.edu>
2015-12-03 23:06:20 -0500

    readUltrasonic

---
+++ readUltrasonic.ino
@@ -0,0 +1,22 @@
+float distanceFront;
+float distanceRight;
+float distanceLeft;
+
+void setup() {}
+
+void loop() {}
+/*
+ * This function reads the ultrasonic values sent over Serial3 from the Arduino Uno.
+ *
+ * This function is used in the findCandle() function and called in the first case.
+ *
+ * inputs: none
+ * outputs: none
+ *
+ */
+void readUltrasonic() {
+
+  distanceFront = Serial3.readStringUntil(',').toFloat();
+  distanceLeft = Serial3.readStringUntil(',').toFloat();
+  distanceRight = Serial3.readStringUntil('\n').toFloat();
+}
