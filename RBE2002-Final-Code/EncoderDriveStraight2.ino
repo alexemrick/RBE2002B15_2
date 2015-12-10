4054d3fb61554a85e729c875b5ecf0a21dd1539b
NicoletteVere <nivere@wpi.edu>
2015-12-10 15:18:51 -0500

    changes

--- RBE2002-Final-Code/EncoderDriveStraight2.ino
+++ RBE2002-Final-Code/EncoderDriveStraight2.ino
@@ -24,8 +24,9 @@ void driveStraight() {
     readUltrasonic();
     POUT = error * kp + DError * kd + IError * ki;
     rightDrive.write(slavePower + POUT);
-    trackDistance();
-    dist
+
+    doTrig(trackdistance(), readGyro());
+    distOrientation(readGyro());
   }
 }
 
